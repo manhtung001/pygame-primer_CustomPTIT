2379e674238e6ce39a7ac6a9367deebde044be8f
manhtung001 <khongtung001@gmail.com>
2021-10-03 23:16:53 +0700

    add stone and clean code

--- PyMenu/char.py
+++ PyMenu/char.py
@@ -2,6 +2,7 @@ import pygame
 import random
 from pygame.locals import *
 
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
@@ -35,24 +36,23 @@ class Player(pygame.sprite.Sprite):
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, type):
         super(Enemy, self).__init__()
-        self.type = type;
-        self.updateIcon();
-        self.image.set_colorkey((255, 255, 255), RLEACCEL)
-        self.rect = self.image.get_rect(
-            center=(random.randint(820, 900), random.randint(0, 600)))
-        self.speed = random.randint(5, 20)
-        self.isFlag = False
-        self.updateHp()
+        self.type = type
 
-    def updateIcon(self):
-        if (self.type == 1):
+        if self.type == "bullet":
             self.image = pygame.image.load('assets/img/missile.png').convert()
+            self.image.set_colorkey((255, 255, 255), RLEACCEL)
+            self.speed = random.randint(5, 10)
+            self.rect = self.image.get_rect(
+                center=(random.randint(820, 900), random.randint(0, 600)))
+            self.weight = 1
 
-    def updateHp(self):
-        if self.type == 1:
-            self.hp_lost = 1
-        if self.type == 2:
-            self.hp_lost = 2
+        if self.type == "stone":
+            self.image = pygame.image.load('assets/img/stone.jpg').convert()
+            self.image.set_colorkey((255, 255, 255), RLEACCEL)
+            self.speed = random.randint(5, 10)
+            self.rect = self.image.get_rect(
+                center=(random.randint(820, 900), random.randint(0, 600)))
+            self.weight = 3
 
     def update(self):
         self.rect.move_ip(-self.speed, 0)
@@ -65,39 +65,19 @@ class Enemy(pygame.sprite.Sprite):
 
 class Item(pygame.sprite.Sprite):
     def __init__(self, type):
-        super(Coin, self).__init__()
-        self.image = pygame.image.load('assets/img/coin.png').convert()
-        self.image.set_colorkey((255, 255, 255), RLEACCEL)
-        self.rect = self.image.get_rect(
-            center=(random.randint(820, 900), random.randint(0, 600))
-        )
-        self.speed = random.randint(5, 10)
+        super(Item, self).__init__()
         self.time_eat_last = 0
         # 100
         self.time_eat_max = 0;
-        if type == 1:
-            self.time_eat_max = 5
+        if type == "coin":
+            self.image = pygame.image.load('assets/img/coin.png').convert()
+            self.speed = random.randint(5, 10)
+            self.image.set_colorkey((255, 255, 255), RLEACCEL)
+            self.rect = self.image.get_rect(
+                center=(random.randint(820, 900), random.randint(0, 600))
+            )
 
-        if type == 2:
-            self.time_eat_max = 4
-
-
-    def update(self):
-        self.rect.move_ip(-self.speed, 0)
-        if self.rect.right < 0:
-            self.kill()
-
-
-
-class Coin(pygame.sprite.Sprite):
-    def __init__(self):
-        super(Coin, self).__init__()
-        self.image = pygame.image.load('assets/img/coin.png').convert()
-        self.image.set_colorkey((255, 255, 255), RLEACCEL)
-        self.rect = self.image.get_rect(
-            center=(random.randint(820, 900), random.randint(0, 600))
-        )
-        self.speed = random.randint(5, 10)
+        # if type == "stone":
 
     def update(self):
         self.rect.move_ip(-self.speed, 0)
--- PyMenu/func.py
+++ PyMenu/func.py
@@ -9,17 +9,19 @@ class Func():
         # nhung cai
 
     def ini_char(self):
-        self.ADD_ENEMY = pygame.USEREVENT + 1
-        pygame.time.set_timer(self.ADD_ENEMY, 250)
-        self.ADD_CLOUD = pygame.USEREVENT + 2
+        self.ADD_BULLET = pygame.USEREVENT + 1
+        pygame.time.set_timer(self.ADD_BULLET, 250)
+        self.ADD_STONE = pygame.USEREVENT + 2
+        pygame.time.set_timer(self.ADD_STONE, 750)
+        self.ADD_CLOUD = pygame.USEREVENT + 4
         pygame.time.set_timer(self.ADD_CLOUD, 1000)
-        self.ADD_COIN = pygame.USEREVENT + 3
+        self.ADD_COIN = pygame.USEREVENT + 5
         pygame.time.set_timer(self.ADD_COIN, 1000)
 
         self.player = Player()
         self.enemies = pygame.sprite.Group()
         self.clouds = pygame.sprite.Group()
-        self.coins = pygame.sprite.Group()
+        self.items = pygame.sprite.Group()
         self.all_sprites = pygame.sprite.Group()
         self.all_sprites.add(self.player)
 
@@ -33,42 +35,42 @@ class Func():
             if event.type == pygame.QUIT:
                 self.running, self.playing = False, False
                 self.curr_menu.run_display = False
-            if event.type == self.ADD_ENEMY:
-                new_enemy = Enemy(1)
-                self.enemies.add(new_enemy)
-                self.all_sprites.add(new_enemy)
+            if event.type == self.ADD_BULLET:
+                new_bullet = Enemy("bullet")
+                self.enemies.add(new_bullet)
+                self.all_sprites.add(new_bullet)
+            if event.type == self.ADD_STONE:
+                new_stone = Enemy("stone")
+                self.enemies.add(new_stone)
+                self.all_sprites.add(new_stone)
             if event.type == self.ADD_CLOUD:
                 new_cloud = Cloud()
                 self.clouds.add(new_cloud)
                 self.all_sprites.add(new_cloud)
             if event.type == self.ADD_COIN:
-                new_coin = Coin()
-                self.coins.add(new_coin)
+                new_coin = Item("coin")
+                self.items.add(new_coin)
                 self.all_sprites.add(new_coin)
 
     def game_update(self):
         self.player.update(pygame.key.get_pressed())
         self.enemies.update()
         self.clouds.update()
-        self.coins.update()
+        self.items.update()
         # render
         for entity in self.all_sprites:
             self.window.blit(entity.image, entity.rect)
 
-        # for enemy in self.enemies:
-        #     if pygame.sprite.spritecollide(self.player, self.enemies, True):
-        #         self.player_hp -= 1
-        #
-        #
-        # for item in self.coins:
-        #     if pygame.sprite.spritecollide(self.player, self.coins, True):
-        #         self.player_score += 1
+        for enemy in self.enemies:
+            if pygame.sprite.collide_rect(self.player, enemy):
+                self.player_hp -= enemy.weight
+                enemy.kill()
 
+        for item in self.items:
+            if pygame.sprite.collide_rect(self.player, item):
+                self.player_score += 1
+                item.kill()
 
-        if pygame.sprite.spritecollide(self.player, self.enemies, True):
-            self.player_hp -= 1
-        if pygame.sprite.spritecollide(self.player, self.coins, True):
-            self.player_score += 1
 
         # isinstance()
 
@@ -98,6 +100,4 @@ class Func():
             self.player.kill()
             self.finish_game()
 
-
             # if pygame.sprite.spritecollideany(self.player, item):
-
