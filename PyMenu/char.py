1e1ec53785fd6f83aeca536bd7941cab6119c25c
manhtung001 <khongtung001@gmail.com>
2021-11-08 23:29:46 +0700

    delete cloud

--- PyMenu/char.py
+++ PyMenu/char.py
@@ -152,16 +152,3 @@ class Item(pygame.sprite.Sprite):
             self.kill()
 
 
-class Cloud(pygame.sprite.Sprite):
-    def __init__(self):
-        super(Cloud, self).__init__()
-        self.image = pygame.image.load('assets/img/cloud.png').convert()
-        self.image.set_colorkey((0, 0, 0), RLEACCEL)
-        self.rect = self.image.get_rect(center=(
-            random.randint(820, 900), random.randint(0, 600))
-        )
-
-    def update(self):
-        self.rect.move_ip(-5, 0)
-        if self.rect.right < 0:
-            self.kill()
